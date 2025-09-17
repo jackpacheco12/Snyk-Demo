8203c24006c647fcd195204f73742c4f951b5b63
Jack Pacheco <jackpacheco@Jacks-MacBook-Air.local>
2025-09-17 10:30:28 -0400

    testing out critical severity fail

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,12 +55,11 @@ resource "google_container_cluster" "bookshelf_cluster" {
   name     = var.cluster_name
   location = var.gcp_region
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
+  
   remove_default_node_pool = true
   initial_node_count       = 1
-
+  
+  #FIXHERE: 
   network    = google_compute_network.bookshelf_vpc.name
   subnetwork = google_compute_subnetwork.bookshelf_subnet.name
 
